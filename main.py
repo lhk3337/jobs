8b70ddd2eb4486811a6bf1d1d8193933d6ef2876
HK <user@HK.local>
2020-04-06 23:15:19 +0900

    extract indeed pages

--- main.py
+++ main.py
@@ -2,3 +2,13 @@ import requests
 from bs4 import BeautifulSoup
 
 indeed_result = requests.get(https: // www.indeed.com/jobs?q=javascript & limit=50)
+
+indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
+
+pagination = indeed_soup.find("div", {"class": "pagination"})
+
+pages = pagination.find_all("a")
+spans = []
+for page in pages:
+    spans.append(page.find("span"))
+spans = spans[:-1]
