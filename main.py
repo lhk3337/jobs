ef1909138f4116d93fb4c798b3fa351df9d3367b
HK <lhk3337@naver.com>
2020-04-06 23:20:31 +0900

    extract indeed page

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 
 indeed_result = requests.get(https: // www.indeed.com/jobs?q=javascript & limit=50)
-
 indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
 
 pagination = indeed_soup.find("div", {"class": "pagination"})
