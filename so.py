820db5f3e571b6579269c121c20b78bc1d7ebf8e
HK <lhk3337@naver.com>
2020-04-07 01:16:26 +0900

    SO extract_job

--- so.py
+++ so.py
@@ -10,15 +10,28 @@ def get_last_page():
     last_pages = pages[-2].get_text(strip=True)
 
 
-def extract_jobs(last_jobs):
+def extract_job(html):
+    title = html.find("a", {"class": "s-link"})["title"]
+    company_txt, location_txt = html.find(
+        "h3", {"class": "mb4"}).find_all("span", recursive=False)
+    company = company_txt.get_text(strip=True)
+    location = location_txt.get_text(strip=True)
+    job_id = html['data-jobid']
+
+    return {"title": title, "company": company, "location": location, "apply_link": f"https://stackoverflow.com/jobs/{job_id}"}
+
+
+def extract_jobs(last_page):
     jobs = []
-    for page in range(last_jobs):
+    for page in range(last_page):
         print(f"Scrapping S0: Page: {page}")
         result = requests.get(f"{URL}&pg={page+1}")
         soup = BeautifulSoup(result.text, "html.parser")
         results = soup.find_all("div", {"class": "-job"})
         for result in results:
-            print(result[data-jobid])
+            job = extract_job(result)
+            jobs.append(job)
+        return jobs
 
 
 def get_jobs():
